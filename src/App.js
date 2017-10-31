53a2beb6820e03610b858ccd148def64f41f5b37
MelanieOC <melanie.ocharan@gmail.com>
2017-10-31 14:44:52 -0500

    votos

--- src/App.js
+++ src/App.js
@@ -1,50 +1,103 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Button, Image, Col, Row, Grid, Navbar, Nav, NavItem } from 'react-bootstrap';
 import * as firebase from 'firebase';
+import { individuales, grupales } from './data.js';
+import {
+  BrowserRouter,
+  Route,
+  NavLink,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 
-class Concursante extends Component {
+const Navs = () => {
+  return (
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1}><NavLink className='navlink' exact to="/">Home</NavLink></NavItem>
+          <NavItem eventKey={2}><NavLink className='navlink' to="/individual">Individual</NavLink></NavItem>
+          <NavItem eventKey={2}><NavLink className='navlink' to="/grupal">Grupal</NavLink></NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+class MostrarParticipantes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      primerDato: 0
+      primerDato: 0,
+      votadoGrupal: false,
+      votadoIndividual: false
     };
   }
   render() {
     const obtenerPuntaje = (opcion) => {
-      firebase.database().ref(opcion).once('value').then((span)=> {
+      firebase.database().ref(opcion).once('value').then((span) => {
         var childKey = span.key;
         var childData = span.val();
         childData++;
-        this.setState(function () {
-          return {
-            primerDato: childData
-          }
-        });
         firebase.database().ref(opcion).set(childData).then(() => {
-          console.log('cuenta actualizada!');
+          if (this.props.array === individuales) {
+            this.setState(function () {
+              return {
+                votadoIndividual: true
+              }
+            });
+          } else {
+            this.setState(function () {
+              return {
+                votadoGrupal: true
+              }
+            });
+          }
+
         });
       });
-
     }
-
-
+    const comparar = this.props.array === individuales ? this.state.votadoIndividual : this.state.votadoGrupal;
     return (
-      <div>
-        <p>{
-          this.state.primerDato
-        }</p>
-        <Button bsStyle="primary" onClick={() => obtenerPuntaje(this.props.opcion)}>
-        {this.props.opcion}</Button>
+      <div style={{ padding: '20px' }}>
+        <Row>
+          {
+            comparar ? <h1>GRACIAS POR VOTAR</h1> :
+              this.props.array.map(item => {
+                return (
+                  <Col md={6} sm={6} className='participante'>
+                    <Image src={item.image} className={item.tipo} circle responsive />
+                    <p className='nombre'>{item.user}</p>
+                    <Button className='voto' bsStyle="primary" bsSize="large" onClick={() => obtenerPuntaje(item.nombre)}>
+                      <i className="fa fa-star" aria-hidden="true"></i> Vote
+                    </Button>
+                  </Col>
+                );
+              })
+          }
+        </Row>
       </div>
     );
   }
 }
-
-
+/*
+<div className='voto'>{
+                    this.state.primerDato
+                  }</div>
+*/
+const Home = () => {
+  return (
+    <div>
+      <h1>Vota por tu favorita</h1>
+    </div>
+  );
+}
 class App extends Component {
-  
+
   componentDidMount() {
     var config = {
       apiKey: "AIzaSyBPqzc7lM12Y1WQCwxDvrXmyiR-SOobb68",
@@ -55,30 +108,29 @@ class App extends Component {
       messagingSenderId: "20735400064"
     };
     firebase.initializeApp(config);
-
   }
 
   render() {
-		/*firebase.database().ref('alumno').on ('value',  function (span) {
-			var childKey = span.key;
-			var childData = span.val();
-			alert('new element'+ childData);
-		});*/
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Concursante opcion={'opcion1'}/>
-        <Concursante opcion={'opcion2'}/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navs />
+          <div className='content'>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/individual" render={() => <MostrarParticipantes array={individuales} />} />
+              <Route path="/grupal" render={() => <MostrarParticipantes array={grupales} />} />
+              <Route render={() => <Redirect to="/" />} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
+
+
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,23 +1,48 @@
+import yolo from './img/yolo.jpg';
+import gaby from './img/gaby.jpg';
+import gladys from './img/gladys.jpg';
+import states from './img/states.jpg';
+import agile from './img/agile.jpg';
+import bertha from './img/bertha.jpg';
+import raiza from './img/raiza.jpg';
+import claushey from './img/clau.jpg';
+import coolcode from './img/coolcode.jpg';
+
 export const individuales = [
     {
         nombre: 'gaby',
-        image: ''
+        user: 'Gaby',
+        image: gaby,
+        id:1,
+        tipo: 'individual'
     },
     {
         nombre: 'gladys',
-        image: ''
+        user: 'Gladys',
+        image: gladys,
+        id:2,
+        tipo: 'individual'
     },
     {
         nombre: 'bertha',
-        image: ''
+        user: 'Bertha',
+        image: bertha,
+        id:3,
+        tipo: 'individual'
     },
     {
         nombre: 'raiza',
-        image: ''
+        user: 'Raiza',
+        image: raiza,
+        id:4,
+        tipo: 'individual'
     },
     {
         nombre: 'clau&shey',
-        image: ''
+        user: 'Shey y Clau',
+        image: claushey,
+        id:5,
+        tipo: 'individual'
     }
 ]
 
@@ -25,14 +50,30 @@ export const individuales = [
 export const grupales = [
     {
         nombre: 'states',
-        image: ''
+        user: 'States',
+        image: states,
+        id:1,
+        tipo: 'grupal'
     },
     {
         nombre: 'coolcode',
-        image: ''
+        user: 'CoolCode',
+        image: coolcode,
+        id:2,
+        tipo: 'grupal'
     },
     {
         nombre: 'agiles',
-        image: ''
+        user: 'Agiles',
+        image: agile,
+        id:3,
+        tipo: 'grupal'
+    },
+    {
+        nombre: 'yolo',
+        user: 'YOLO',
+        image: yolo,
+        id:4,
+        tipo: 'grupal'
     }
 ]
